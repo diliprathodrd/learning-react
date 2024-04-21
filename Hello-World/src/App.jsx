a83a2415506c607533b8f0d24ba1adff464c46e4
diliprathodrd <diliprathodofficial@gmail.com>
2024-04-21 22:28:34 +0530

    hello world project

---
+++ Hello-World/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Name from "./Name";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Name />
+    </>
+  );
+}
+
+export default App;
